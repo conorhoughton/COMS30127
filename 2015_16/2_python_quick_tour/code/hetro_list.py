c1ea55b253e667484146f5d1b3487d1a62028f2a
Conor <conor.houghton@bristol.ac.uk>
2016-02-02 15:44:34 +0000

    python from last year, mild updates
    Former-commit-id: 1eb6bdb773b554d2cd77c3564603ad9c27ef9cfe

---
+++ 2015_16/2_python_quick_tour/code/hetro_list.py
@@ -0,0 +1,2 @@
+list=[72,79,86,96,103,"Catheral Parkway"]
+print list
---
+++ 2015_16/2_python_quick_tour/code/np_matrix.py
@@ -0,0 +1,6 @@
+import numpy as np
+d1=np.array([(1,-1,1),(1,2,1),(1,-1,1)])
+print d1
+d2=np.array([1,3,1])
+print d2
+print np.dot(d1,d2)

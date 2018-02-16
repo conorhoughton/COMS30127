f507d36c8a7d174dae74a3758cf3768374df022d
Conor Houghton <cscjh@localhost.localdomain>
2018-02-16 00:36:25 +0000

    draft of cw

---
+++ 2017_18/coursework/auto/seven_segments.jl
@@ -0,0 +1,80 @@
+
+function seven_segment(pattern::Array{Int64})
+
+    function to_bool(a::Int64)
+        if a==1
+            return true
+        end
+        false
+    end
+
+    function hor(d)
+        if d
+            println(" _ ")
+        else
+            println("   ")
+        end
+    end
+    function vert(d1,d2,d3)
+        word=""::String
+        if d1
+            word="|"
+        else
+            word=" "
+        end
+        if d3
+            word=string(word,"_")
+        else
+            word=string(word," ")
+        end
+        if d2
+            word=string(word,"|")
+        else
+            word=string(word," ")
+        end 
+        println(word)
+
+    end
+
+    pattern_b=map(to_bool,pattern)
+
+    hor(pattern_b[1])
+    vert(pattern_b[2],pattern_b[3],pattern_b[4])
+    vert(pattern_b[5],pattern_b[6],pattern_b[7])
+
+    number=0
+    for i in 1:4
+        if pattern_b[7+i]
+            number+=2^(i-1)
+        end
+    end
+
+    println(number)
+        
+end
+
+six=Int64[1,1,-1,1,1,1,1,-1,1,1,-1]
+three=Int64[1,-1,1,1,-1,1,1,1,1,-1,-1]
+one=Int64[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
+
+seven_segment(three)
+seven_segment(six)
+seven_segment(one)
+
+#------------------
+
+println("test1")
+
+test=Int64[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+println("test2")
+
+#here the network should run printing at each step
+
+test=Int64[1,1,1,1,1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+#here the network should run printing at each step
---
+++ 2017_18/coursework/auto/seven_segments.py
@@ -0,0 +1,76 @@
+from math import *
+
+def seven_segment(pattern):
+
+    def to_bool(a):
+        if a==1:
+            return True
+        return False
+    
+
+    def hor(d):
+        if d:
+            print(" _ ")
+        else:
+            print("   ")
+    
+    def vert(d1,d2,d3):
+        word=""
+
+        if d1:
+            word="|"
+        else:
+            word=" "
+        
+        if d3:
+            word+="_"
+        else:
+            word+=" "
+        
+        if d2:
+            word+="|"
+        else:
+            word+=" "
+        
+        print(word)
+
+    
+
+    pattern_b=map(to_bool,pattern)
+
+    hor(pattern_b[0])
+    vert(pattern_b[1],pattern_b[2],pattern_b[3])
+    vert(pattern_b[4],pattern_b[5],pattern_b[6])
+
+    number=0
+    for i in range(0,4):
+        if pattern_b[7+i]:
+            number+=pow(2,i)
+    print(int(number))
+        
+
+
+six=[1,1,-1,1,1,1,1,-1,1,1,-1]
+three=[1,-1,1,1,-1,1,1,1,1,-1,-1]
+one=[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
+
+seven_segment(three)
+seven_segment(eight)
+seven_segment(one)
+
+
+print("test1")
+
+test=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+print("test2")
+
+#here the network should run printing at each step
+
+test=[1,1,1,1,1,1,1,-1,-1,-1,-1]
+
+seven_segment(test)
+
+#here the network should run printing at each step
